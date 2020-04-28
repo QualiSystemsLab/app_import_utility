e6c3dc430cda8f49583197a42317cefe5d2d0cd1
DYeag <37943859+DYeag@users.noreply.github.com>
2020-04-28 08:40:50 -0700

    Create save_app_info_orch_helper.py
    moved app save orchestration helper funtions to it's own file

---
+++ python packages/cloudshell-app-helper-1.0.0/cloudshell/helpers/apps/save_app_info_orch_helper.py
@@ -0,0 +1,27 @@
+import json
+
+from cloudshell.api.cloudshell_api import SandboxDataKeyValue
+
+
+def create_json_string(deployment_paths):
+    result = dict()
+    result['deploypaths'] = []
+    for deploypath in deployment_paths:
+        path = dict()
+        path['name'] = deploypath.Name
+        path['service_name'] = deploypath.DeploymentService.Name
+        path['attributes'] = dict()
+        for attribute in deploypath.DeploymentService.Attributes:
+            path['attributes'][attribute.Name] = attribute.Value
+        result['deploypaths'].append(path)
+
+    return json.dumps(result)
+
+
+def save_app_deployment_info(sandbox, components=None):
+    for app in sandbox.automation_api.GetReservationDetails(sandbox.id).ReservationDescription.Apps:
+        jsonstring = create_json_string(app.DeploymentPaths)
+
+        key_value = SandboxDataKeyValue(app.Name, jsonstring)
+
+        sandbox.automation_api.SetSandboxData(sandbox.id, [key_value])
